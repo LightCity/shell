e723b7afea171c205e0f03b4b52c32612e13e02d
Longli <longcpp@gmail.com>
2018-04-03 12:23:51 +0800

    for each

---
+++ fuck.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+for subdir in $(ls -p .)
+do
+    #echo ${subdir}
+    if test -d ${subdir} && [ -e ${subdir}/.git ]; then
+        cd ${subdir}
+        pwd
+        if [ -n "$1" ]; then
+            eval $1
+        else
+            # 默认操作
+            git remote -v
+        fi
+        cd ..
+	else
+		echo "\t\t目录\"${subdir}\"不是有效的git项目"
+    fi
+    echo '\n'
+done
